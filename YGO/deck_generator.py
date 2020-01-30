bcac41018bed330dd4e9fa58632ead6d8ccf0f36
Gao Zhong <2587291794@qq.com>
2020-01-30 16:06:45 +0800

    Add files via upload

--- YGO/deck_generator.py
+++ YGO/deck_generator.py
@@ -1,33 +1,47 @@
-# import cdblib
-import os
 import random
 import time
 from tkinter import *
 
 
-def get_card_data(key_path):
-    # Read cards
-    # cdb_path = key_path + "cdb\\cards.cdb"
-    # with open(cdb_path, 'rb') as cdb_file:
-    #     cdb_data = cdb_file.read()
-    # reader = cdblib.Reader(cdb_data)
-    #
-    # for key, value in reader.iteritems():
-    #     print('+{},{}:{}->{}'.format(len(key), len(value), key, value))
-    pic_path = key_path + "\\picture\\card\\"
-    card_name_list = os.listdir(pic_path)
-    card_name_list = list(map(lambda x: x[:-4], card_name_list))
-    return card_name_list
+def get_cards_data():
+    with open("cards.txt", 'rb') as file:
+        data = file.readlines()
+    series_dict = {}
+    for line in data:
+        line = str(line)
+        if 'INSERT or replace into datas values' in line:
+            line_index_1 = line.find('(')
+            line_index_2 = line.find(')')
+            line_data = line[line_index_1 + 1:line_index_2].split(',')
+            if not line_data[3] == '0x0':
+                epochs = int(len(line_data[3]) / 2 - 1)
+                for epoch in range(epochs):
+                    series = line_data[3][2 * epoch + 2:2 * epoch + 4]
+                    if not series == '00':
+                        if series in series_dict.keys():
+                            series_dict[series].append(line_data[0])
+                        else:
+                            series_dict[series] = [line_data[0]]
+    random_pool = {}
+    for key in series_dict.keys():
+        if len(series_dict[key]) > 13:
+            random_pool[key] = series_dict[key]
+    return random_pool
 
 
 def generate_deck_file():
     key_path = key_path_entry.get()
     key_path.replace('\\', "\\\\")
+    series_name = series_name_entry.get()
     deck_name = deck_name_entry.get()
     deck_length = int(deck_length_entry.get())
 
     # Generate the deck content
-    random_cards = random.sample(get_card_data(key_path), deck_length)
+    card_pool = series_data[series_name]
+    random_cards = []
+    while len(random_cards) < deck_length:
+        random_cards += random.sample(card_pool, 2)
+
     deck = "#created by ygopro2\n#main\n" + "\n".join(random_cards) + "#extra\n!side\n"
     with open(key_path + "\\deck\\" + deck_name, 'w') as deck_file:
         deck_file.write(str(deck))
@@ -35,18 +49,21 @@ def generate_deck_file():
     refresh()
 
 
-def entry_set(entry, value):
-    entry.delete(0, END)
-    entry.insert(0, value)
-
-
 def refresh():
-    entry_set(series_name_entry, "Temporarily useless")
-    refresh_name = time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time())) + '.ydk'
-    entry_set(deck_name_entry, refresh_name)
+    def entry_set(entry, value):
+        entry.delete(0, END)
+        entry.insert(0, value)
+
+    new_series_name = random.sample(series_data.keys(), 1)
+    entry_set(series_name_entry, new_series_name)
+
+    new_deck_name = time.strftime('%m%d', time.localtime(time.time())) + ''.join(new_series_name) + '.ydk'
+    entry_set(deck_name_entry, new_deck_name)
 
 
 if __name__ == "__main__":
+    series_data = get_cards_data()
+
     deck_generator = Tk()
     deck_generator.title("YGO卡组生成器")
 
@@ -55,11 +72,12 @@ if __name__ == "__main__":
     key_path_entry.grid(column=2, row=1, sticky=(W, E))
 
     Label(deck_generator, text="系列名称:").grid(column=1, row=11, sticky=(W, E))
-    series_name_entry = Entry(deck_generator, width=25, textvariable=StringVar(value="Temporarily useless"))
+    random_series_name = random.sample(series_data.keys(), 1)
+    series_name_entry = Entry(deck_generator, width=25, textvariable=StringVar(value=random_series_name))
     series_name_entry.grid(column=2, row=11)
 
     Label(deck_generator, text="生成卡组名称:").grid(column=1, row=12, sticky=(W, E))
-    default_name = time.strftime('%Y%m%d-%H%M%S', time.localtime(time.time())) + '.ydk'
+    default_name = time.strftime('%m%d', time.localtime(time.time())) + ''.join(random_series_name) + '.ydk'
     deck_name_entry = Entry(deck_generator, width=25, textvariable=StringVar(value=default_name))
     deck_name_entry.grid(column=2, row=12)
 
