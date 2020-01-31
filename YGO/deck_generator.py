ecc5013d9165516b7f2a4913646b1ae19345cc6c
Gao Zhong <2587291794@qq.com>
2020-01-31 15:53:36 +0800

    Add files via upload

--- YGO/deck_generator.py
+++ YGO/deck_generator.py
@@ -3,20 +3,55 @@ F:
 cd F:\01 Codes\Python\YuGiOh
 pyinstaller -Fw deck_generator.py
 """
+import copy
 import random
 import time
 from tkinter import *
 
+# Todo: 同名异画卡
 
-def analysis_cards_cdb(set_name_path, card_path):
-    def dictionary_append(dictionary, key, value):
-        if key in dictionary.keys():
-            dictionary[key].append(value)
-        else:
-            dictionary[key] = [value]
+joint_set = """
+弹丸 枪管
+圣 阿托利斯 兰斯洛特
+破坏剑 破坏之剑士
+艾克佐迪亚 被封印
+虫惑 洞 落穴
+守墓 王家长眠之谷
+六武众 影六武众 紫炎
+魔术师 灵摆
+"""
+remove_set = """
+融合
+升阶魔法
+帝王
+"""
+
+
+def dictionary_append(dictionary, key, value):
+    if key in dictionary.keys():
+        dictionary[key].append(value)
+    else:
+        dictionary[key] = [value]
+
+
+def prepare_analysis():
+    universal_items = []
+    with open("universal.ydk", 'r', encoding='utf-8') as universal_file:
+        universal_data = universal_file.readlines()
+    for universal_line in universal_data[2:]:
+        if not ('extra' in universal_line or 'side' in universal_line):
+            universal_items.append(universal_line.replace('\n', ''))
+
+    joint_items = []
+    for joint_line in joint_set.split('\n'):
+        joint_items.append(joint_line.split(' '))
+
+    remove_items = []
+    for remove_line in remove_set.split('\n'):
+        remove_items.append(remove_line)
 
     series_name_dict = {}
-    with open(set_name_path, "r", encoding='utf-8') as set_name_file:
+    with open("set_name.txt", "r", encoding='utf-8') as set_name_file:
         set_name_data = set_name_file.readlines()
     for set_name_line in set_name_data[1:]:
         # if '#' not in set_name_line:
@@ -24,11 +59,16 @@ def analysis_cards_cdb(set_name_path, card_path):
         series_name = line_data[2].split('\t')[0].replace('\n', '')
         if eval(line_data[1]) < 0xfff:
             series_name_dict[line_data[1]] = series_name
+    return universal_items[1:-1], joint_items[1:-1], remove_items[1:-1], series_name_dict
 
-    with open(card_path, 'rb') as card_file:
-        card_data = card_file.readlines()
+
+def get_analysis_result():
+    universal_items, joint_items, remove_items, series_name_dict = prepare_analysis()
+
     card_dict = {}
     series_dict = {}
+    with open("cards.txt", 'rb') as card_file:
+        card_data = card_file.readlines()
     for card_line in card_data:
         card_line = str(card_line)
         if 'INSERT or replace into datas values' in card_line:
@@ -38,32 +78,29 @@ def analysis_cards_cdb(set_name_path, card_path):
 
             card_number = line_data[0]
             allowed_quantity = eval(line_data[1])
-            set_name_index = line_data[3]
             card_type = eval(line_data[4])
 
-            if allowed_quantity == 0 or eval(set_name_index) == 0 or card_type & 0x4000:  # or card_type & 0x8008:
+            if allowed_quantity == 0 or card_type & 0x4000:  # or card_type & 0x8008:
+                continue
+
+            if card_number in universal_items:
+                card_dict[card_number] = [card_type & 0x4802040, allowed_quantity]
+                dictionary_append(series_dict, '泛用', card_number)
+
+            set_name_index = line_data[3]
+            if eval(set_name_index) == 0:
                 continue
 
             if eval(set_name_index) > 0xfff:
                 set_name_index = str(hex(eval('0x' + set_name_index[-3:])))
 
-            if card_type & 0x4802040:
-                card_dict[card_number] = [True, allowed_quantity, set_name_index]
-            else:
-                card_dict[card_number] = [False, allowed_quantity, set_name_index]
-
+            card_dict[card_number] = [card_type & 0x4802040, allowed_quantity]
             if set_name_index in series_name_dict.keys():
                 set_name = series_name_dict[set_name_index]
                 dictionary_append(series_dict, set_name, card_number)
             else:
-                print(card_number, set_name_index)
                 dictionary_append(series_dict, set_name_index, card_number)
 
-    joint_items = []
-    with open("joint_set.txt", "r", encoding='utf-8') as joint_file:
-        joint_data = joint_file.readlines()
-    for joint_line in joint_data:
-        joint_items.append(joint_line.replace('\n', '').split('\t'))
     for joint_item in joint_items:
         key_item = joint_item[0]
         for sub_item in joint_item[1:]:
@@ -71,55 +108,80 @@ def analysis_cards_cdb(set_name_path, card_path):
                 series_dict[key_item] = series_dict[key_item] + series_dict[sub_item]
                 del series_dict[sub_item]
 
-    random_pool = {}
-    for set_name in series_dict.keys():
-        if len(series_dict[set_name]) > 5:
-            random_pool[set_name] = series_dict[set_name]
+    for remove_item in remove_items:
+        del series_dict[remove_item]
 
     return card_dict, series_dict
 
 
 def generate_deck_file():
+    def add_one_card(pool, main, extra, side):
+        random_card_name = random.sample(pool, len(pool))[0]
+        extra_flag, allowed_quantity = card_data_dict[random_card_name]
+        side_count = side.count(random_card_name)
+        if extra_flag:
+            extra_count = extra.count(random_card_name)
+            if extra_count + side_count < allowed_quantity:
+                if len(extra) < 15:
+                    extra.append(random_card_name)
+                elif len(side) < 15:
+                    side.append(random_card_name)
+                else:
+                    pool.remove(random_card_name)
+            else:
+                pool.remove(random_card_name)
+        else:
+            main_count = main.count(random_card_name)
+            if main_count + side_count < allowed_quantity:
+                if len(main) < deck_length:
+                    main.append(random_card_name)
+                elif len(side) < 15:
+                    side.append(random_card_name)
+                else:
+                    pool.remove(random_card_name)
+            else:
+                pool.remove(random_card_name)
+        return extra_flag, random_card_name
+
     key_path = key_path_entry.get()
     key_path.replace('\\', "\\\\")
     series_name = series_name_entry.get()
     deck_name = deck_name_entry.get()
     deck_length = int(deck_length_entry.get())
 
-    card_pool = series_random_pool[series_name]
     main_cards = []
     extra_cards = []
-    side_card = []
+    side_cards = []
+    universal_card = copy.deepcopy(series_random_pool['泛用'])
+    card_pool = copy.deepcopy(series_random_pool[series_name])
+
     while (len(main_cards) < deck_length or len(extra_cards) < 15) and len(card_pool) > 0:
-        random_card_name = random.sample(card_pool, 1)[0]
-        extra, allowed_quantity, _ = card_data_dict[random_card_name]
-        if extra:
-            extra_cards_count = extra_cards.count(random_card_name)
-            if len(extra_cards) < 15 and extra_cards_count < allowed_quantity:
-                extra_cards.append(random_card_name)
-            else:
-                card_pool.remove(random_card_name)
-        else:
-            main_cards_count = main_cards.count(random_card_name)
-            side_cards_count = side_card.count(random_card_name)
-            if main_cards_count + side_cards_count < allowed_quantity:
-                if len(main_cards) < deck_length:
-                    main_cards.append(random_card_name)
-                elif len(side_card) < 15:
-                    side_card.append(random_card_name)
-                else:
-                    card_pool.remove(random_card_name)
-            else:
-                card_pool.remove(random_card_name)
+        add_one_card(card_pool, main_cards, extra_cards, side_cards)
+
+    if len(extra_cards) == 0:
+        if len(main_cards) + 4 > deck_length:
+            main_cards = main_cards[:deck_length - 4]
+        # 超魔导
+        main_cards.extend(['46986414', '74677422', '6172122'])
+        extra_cards.extend(['37818794', '70369116', ])
+        # 拷问
+        main_cards.append('75732622')
+        extra_cards.extend(['28776350', '98978921', '98978921'])
+        extra_cards.extend(['99111753', '41999284', '41999284'])
+
+    while (len(main_cards) < deck_length or len(extra_cards) < 15) and len(universal_card) > 0:
+        is_extra, random_card = add_one_card(universal_card, main_cards, extra_cards, side_cards)
+        if is_extra and random_card in universal_card:
+            universal_card.remove(random_card)
 
     deck = "#created by ygopro2"
     deck += "\n#main\n" + "\n".join(main_cards)
     deck += "\n#extra\n" + "\n".join(extra_cards)
-    deck += "\n!side\n" + "\n".join(side_card)
+    deck += "\n!side\n" + "\n".join(side_cards)
     with open(key_path + "\\deck\\" + deck_name, 'w') as deck_file:
         deck_file.write(str(deck))
 
-    print(deck_name, len(card_pool), len(main_cards), len(extra_cards), len(side_card))
+    print(deck_name, len(card_pool), len(main_cards), len(extra_cards), len(side_cards))
 
 
 def refresh():
@@ -130,12 +192,14 @@ def refresh():
     new_series_name = random.sample(series_random_pool.keys(), 1)[0]
     entry_set(series_name_entry, new_series_name)
 
-    new_deck_name = time.strftime('%y%m%d', time.localtime(time.time()))[2:] + new_series_name + '.ydk'
+    new_deck_name = time.strftime('%y%m%d', time.localtime(time.time())) + new_series_name + '.ydk'
+    if '|' in new_deck_name:
+        new_deck_name = new_deck_name.split('|')[0] + '.ydk'
     entry_set(deck_name_entry, new_deck_name)
 
 
 if __name__ == "__main__":
-    card_data_dict, series_random_pool = analysis_cards_cdb("set_name.txt", "cards.txt")
+    card_data_dict, series_random_pool = get_analysis_result()
     deck_generator = Tk()
     deck_generator.title("YGO卡组生成器")
 
